2735f469167dd4edfb21946b5b7c28d224714bca
Kacper <newuser@DESKTOP-7F09I1K>
2024-06-24 19:09:06 +0200

    counter

--- src/bootcamp_frontend/src/App.vue
+++ src/bootcamp_frontend/src/App.vue
@@ -1,18 +1,3 @@
-<script setup>
-import { ref } from 'vue';
-import { bootcamp_backend } from 'declarations/bootcamp_backend/index';
-let greeting = ref('');
-
-async function handleSubmit(e) {
-  e.preventDefault();
-  const target = e.target;
-  const name = target.querySelector('#name').value;
-  await bootcamp_backend.greet(name).then((response) => {
-    greeting.value = response;
-  });
-}
-</script>
-
 <template>
   <main>
     <img src="/logo2.svg" alt="DFINITY logo" />
@@ -23,6 +8,25 @@ async function handleSubmit(e) {
       <input id="name" alt="Name" type="text" />
       <button type="submit">Click Me!</button>
     </form>
-    <section id="greeting">{{ greeting }}</section>
+    <section id="greeting">cześć !!!!{{ greeting }}</section>
+  <br/>
+  <button @click="podbij">podbij</button>
+    {{ licznik }}
+    
   </main>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      licznik: 0
+    }
+  },
+  methods: {
+    podbij() {
+      this.licznik++
+    }
+  }
+}
+</script>
